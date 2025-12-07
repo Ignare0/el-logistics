000e24c62a5351702207dbf999b154fa0b7fd7d7
Dustpaw <1838797765@qq.com>
2025-12-08 00:46:01 +0800

    后端增加了查询和操作的接口

--- apps/server/src/types/internal.ts
+++ apps/server/src/types/internal.ts
@@ -1,17 +1,16 @@
-import { Order, OrderStatus, TimelineEvent } from '@el/types';
+// server/src/types/internal.ts
+
+import { Order, OrderStatus, TimelineEvent } from '@el/types'; // 修改这里
 import { LogisticsNode } from '../domain/Node';
 
-// 继承共享的 Order
 export interface ServerOrder extends Order {
     logistics: Order['logistics'] & {
-        // 增加这两个字段，用于记录当前实时位置
-        // 这样前端刷新页面时，可以从这里读取最新位置，而不是从头开始
         currentLat?: number;
         currentLng?: number;
-
-        // 路径规划数据
         plannedRoute?: LogisticsNode[];
     };
+
+    timeline: TimelineEvent[];
 }
 
 export { OrderStatus };
